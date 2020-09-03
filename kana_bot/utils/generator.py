f5fc9efb720820ee39904c633c2b94be6768ec5c
Iqrar Agalosi Nureyza <misteriqrar@gmail.com>
2020-09-03 11:21:00 +0700

    Implement quiz creation for hiragana session type 1

--- kana_bot/utils/generator.py
+++ kana_bot/utils/generator.py
@@ -1,8 +1,14 @@
+import random
 from .kana_bot_error import KanaTypeError
+from .letters import JLetters
 
 
 class QuizGenerator(object):
     """Class to generate questions for quiz."""
+    jletters = JLetters()
+    QUESTION_LIMIT = 20  # the number of question to be displayed
+    question_list = []
+    answer_list = []
 
     def __init__(self, kana_type: str, quiz_type: int):
         self.kana_type = kana_type
@@ -25,13 +31,37 @@ class QuizGenerator(object):
                 f"Got '{kana_type}' instead.")
 
     def create_hiragana_quiz1(self):
-        pass
+        """Hiragana to Romaji quiz."""
+
+        all_letters = self.jletters.get_all_hiragana()
+        taken = set()  # avoid taking the same letter for the questions
+        while len(taken) != self.QUESTION_LIMIT:
+            letter = random.choice(all_letters)
+            if letter not in taken:
+                taken.add(letter)
+
+        self.question_list = list(taken)
+        for i in range(self.QUESTION_LIMIT):
+            answer = self.jletters.get_romaji(self.question_list[i])
+            self.answer_list.append(answer)
 
     def create_hiragana_quiz2(self):
+        """Romaji to Hiragana"""
+
         pass
 
     def create_katakana_quiz1(self):
+        """Katakana to Romaji"""
+
         pass
 
     def create_katakana_quiz2(self):
+        """Romaji to Katakana"""
+
         pass
+
+
+if __name__ == '__main__':
+    g = QuizGenerator("HIRAGANA", 1)
+    print(g.question_list)
+    print(g.answer_list)
